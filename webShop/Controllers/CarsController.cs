de7379e45b8beffa7a101d0deb9728a272ad9816
Ольга Тома < @DESKTOP-9MIIC1N>
2022-06-18 18:15:01 +0300

    ..

--- webShop/Controllers/CarsController.cs
+++ webShop/Controllers/CarsController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using webShop.Data.interfaces;
+using webShop.Data.Models;
 using webShop.ViewModels;
 
 namespace webShop.Controllers
@@ -14,14 +18,40 @@ namespace webShop.Controllers
             _allCategories = iCarsCat;
         }
 
-        public ViewResult List() {
+        [Route("Cars/List")]
+        [Route("Cars/List/{category}")]
+        public ViewResult List(string category) {
+            string _category = category;
+            IEnumerable<Car> cars = null;
+            string currCategory = "";
+            if (string.IsNullOrEmpty(category))
+            {
+                cars = _allCars.Cars.OrderBy(i => i.id);
+            }
+            else {
+                if(string.Equals("electro", category, StringComparison.OrdinalIgnoreCase))
+                {
+                    cars = _allCars.Cars.Where(i => i.Category.categoryName.Equals("Електромобілі")).OrderBy(i => i.id);
+                    currCategory = "Електромобілі";
+                }
+                else if(string.Equals("fuel", category, StringComparison.OrdinalIgnoreCase))
+                {
+                    cars = _allCars.Cars.Where(i => i.Category.categoryName.Equals("Класичні автомобілі")).OrderBy(i => i.id);
+                    currCategory = "Класичні автомобілі";
+                }
+
+            }
+
+            var carObj = new CarsListViewModel
+            {
+                allCars = cars,
+                currCategory = currCategory
+            };
+
             ViewBag.Title = "Сторінка з авто";
-            CarsListViewModel obj = new CarsListViewModel();
-            obj.allCars = _allCars.Cars;
-            obj.currCategory = "Автомобілі";
 
 
-            return View(obj);
+            return View(carObj);
         }
 
 
