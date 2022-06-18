2b61c6e2b4037f5f559be8db16d4772f04d81bd9
Ольга Тома < @DESKTOP-9MIIC1N>
2022-06-18 16:51:03 +0300

    add homecontroller

---
+++ webShop/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using webShop.Data.interfaces;
+using webShop.ViewModels;
+
+namespace webShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IAllCars _carRep;
+
+
+        public HomeController(IAllCars carREp)
+        {
+            this._carRep = carREp;
+        }
+
+        public ViewResult Index()
+        {
+            var homeCars = new HomeViewModel
+            {
+                favCars = _carRep.getFavCars
+            };
+            return View(homeCars);
+        }
+    }
+}
