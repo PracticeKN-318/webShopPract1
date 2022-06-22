e2c85096bc7161d5a462c183e87fe52d25db3835
russu1337 <nazarrussu8@gmail.com>
2022-06-22 14:17:40 +0300

    close//

---
+++ webShop/Migrations/20220622111334_Webshop.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace webShop.Migrations
+{
+    public partial class Webshop : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
